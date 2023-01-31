9a53bdf9eeb26bd1482f2499b2eccb9bc8184529
pulkit171112236 <pulkit171112236@gmail.com>
2023-02-01 01:27:33 +0530

    feature: fixture page design

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -1,119 +1,188 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import EntityContext from '../store/entity-context'
 import './overview.css'
-import Nav from 'react-bootstrap/Nav'
-import Table from 'react-bootstrap/Table'
-import Tab from 'react-bootstrap/Tab'
-import Avatar from '@mui/material/Avatar'
-// reactstrap components
 import './Fixtures.css'
-import ImportExportIcon from '@mui/icons-material/ImportExport'
-import SortIcon from '@mui/icons-material/Sort'
 
-import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Label,
-  FormGroup,
-  Input,
-  Row,
-  Col,
-  UncontrolledTooltip,
-  CardText,
-  CardFooter,
-  Badge,
-  Progress,
-  CardSubtitle,
-  image,
-} from 'reactstrap'
+import Nav from 'react-bootstrap/Nav'
+import Tab from 'react-bootstrap/Tab'
+import { Button, Row, Col, Container, Card } from 'reactstrap'
 import Box from '@mui/material/Box'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-function Fixtures() {
-  const [round, setRound] = useState()
+import Avatar from '@mui/material/Avatar'
 
+const classes = {
+  box: {
+    display: 'flex',
+    margin: '2rem 0',
+    padding: '1rem',
+    boxShadow: 2,
+    borderRadius: 2,
+    background: '#27293d',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: '1.5rem',
+    width: '65%',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    background: 'grey',
+    borderRadius: '7rem',
+    color: 'black',
+    height: '4rem',
+  },
+  button: {
+    textTransform: 'uppercase',
+    margin: '0 auto',
+    '&:hover': 'none',
+  },
+  card: {
+    // background: 'transparent',
+    boxShadow: 'none',
+    width: '40%',
+  },
+  avatar: {
+    width: 200,
+    height: 200,
+    fontSize: '5rem',
+    marginBottom: '1rem',
+  },
+}
+
+const Fixtures = () => {
   return (
-    <Tab.Container id="left-tabs-example" defaultActiveKey="tab1">
-      <Row>
-        <Nav
-          fill
-          variant="pills"
-          classNameName=""
-          style={{ flex: 'auto', marginBottom: '40px' }}
-        >
-          {/* {round.length != 0 && */}
-          {/* round.map((item) => ( */}
-          <Nav.Item>
-            {/* <Nav.Link eventKey="tab1">{item.id}</Nav.Link> */}
-            <Nav.Link eventKey="tab1">Round1</Nav.Link>
-            {/* <Nav.Link eventKey="tab2">Round2</Nav.Link> */}
-          </Nav.Item>
-          <Row>
-            <Col>
-              <Tab.Content>
-                <Tab.Pane eventKey="tab1">
-                  <Row md="12">
-                    {/* <Col md="4"> */}
-                    <div
-                      className="matchdetails card-width"
-                      data-country="Chennai Super Kings, Kolkata Knight Riders"
-                      data-venue="Wankhede Stadium, Mumbai"
+    <div className="content mainContent container">
+      <Tab.Container id="left-tabs-example" defaultActiveKey="tab2">
+        <Row>
+          <Nav
+            fill
+            variant="pills"
+            className=""
+            style={{ flex: 'auto', marginBottom: '40px' }}
+          >
+            <Nav.Item>
+              <Nav.Link eventKey="tab1">Round1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="tab2">Round2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Row>
+        <Row>
+          <Col>
+            <Tab.Content>
+              <Tab.Pane eventKey="tab1">
+                <Box sx={classes.box}>
+                  <Box sx={classes.details}>
+                    <Button
+                      variant="contained"
+                      color="info"
+                      style={classes.button}
                     >
-                      <div className="match-teams">Sat, 4 FEB 2023</div>
-                      <div className="match-venue">Ranbhumi Cricket Ground</div>
-                      <div className="match-meta">Match 1 -9:00 IST</div>
-                      <div className="match-teams">
-                        FORTRA RIDERS VS FORTRA SUPERNOVAS
-                      </div>
-                      <div className="div-text">
-                        <div style={{ display: 'contents' }}>
-                          <Avatar
-                            className="center"
-                            alt={'teamname'}
-                            // src={`${BASE_URL}/${team.imageUrl}`}
-                            src={''}
-                            sx={{
-                              width: 50,
-                              height: 50,
-                              fontSize: '5rem',
-                            }}
-                          />
-                          FORTRA RIDERS
-                        </div>
-                        <div className="circle-text">V/S</div>
-                        <div style={{ display: 'contents' }}>
-                          <Avatar
-                            className="center"
-                            alt={'teamname'}
-                            // src={`${BASE_URL}/${team.imageUrl}`}
-                            src={''}
-                            sx={{
-                              width: 50,
-                              height: 50,
-                              fontSize: '5rem',
-                            }}
-                          />
-                          FORTRA SUPERNOVAS
-                        </div>
-                      </div>
-                    </div>
-                    {/* </Col> */}
-                  </Row>
-                </Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-          {/* ))} */}
-        </Nav>
-      </Row>
-    </Tab.Container>
+                      Match 1
+                    </Button>
+
+                    <Typography variant="h5" style={{ color: 'yellow' }}>
+                      Sunday, 4th Feb 2022
+                    </Typography>
+                    <Typography variant="h5" style={{ color: 'yellow' }}>
+                      4:00 PM
+                    </Typography>
+
+                    <Button
+                      variant="contained"
+                      style={classes.button}
+                      size="lg"
+                    >
+                      Ground : Ranbhumi
+                    </Button>
+                  </Box>
+                  <Card style={classes.card}>
+                    <Avatar
+                      className="center"
+                      src={`anupama-shamshers.jfif`}
+                      sx={classes.avatar}
+                    />
+                    <Typography variant="h5">North Star Smashers</Typography>
+                  </Card>
+
+                  <Typography
+                    variant="h4"
+                    style={{ margin: 'auto', fontWeight: 'bold' }}
+                  >
+                    V/S
+                  </Typography>
+                  <Card style={classes.card}>
+                    <Avatar
+                      className="center"
+                      src={`spartans- Shinja.jpg`}
+                      sx={classes.avatar}
+                    />
+
+                    <Typography variant="h5">Nort Star Spartans</Typography>
+                  </Card>
+                </Box>
+
+                <Box sx={classes.box}>
+                  <Box sx={classes.details}>
+                    <Button
+                      variant="contained"
+                      color="info"
+                      style={classes.button}
+                    >
+                      Match 1
+                    </Button>
+
+                    <Typography variant="h5" style={{ color: 'yellow' }}>
+                      Sunday, 4th Feb 2022
+                    </Typography>
+                    <Typography variant="h5" style={{ color: 'yellow' }}>
+                      4:00 PM
+                    </Typography>
+
+                    <Button
+                      variant="contained"
+                      style={classes.button}
+                      size="lg"
+                    >
+                      Ground : Ranbhumi
+                    </Button>
+                  </Box>
+                  <Card style={classes.card}>
+                    <Avatar
+                      className="center"
+                      src={`anupama-shamshers.jfif`}
+                      sx={classes.avatar}
+                    />
+                    <Typography variant="h5">North Star Smashers</Typography>
+                  </Card>
+
+                  <Typography
+                    variant="h4"
+                    style={{ margin: 'auto', fontWeight: 'bold' }}
+                  >
+                    V/S
+                  </Typography>
+                  <Card style={classes.card}>
+                    <Avatar
+                      className="center"
+                      src={`spartans- Shinja.jpg`}
+                      sx={classes.avatar}
+                    />
+
+                    <Typography variant="h5">Nort Star Spartans</Typography>
+                  </Card>
+                </Box>
+              </Tab.Pane>
+              <Tab.Pane eventKey="tab2"></Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
   )
 }
 
