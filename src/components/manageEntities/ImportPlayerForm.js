be4ab244373bb02cc1fe3352043f0bcc2a02e90d
pulkit171112236 <pulkit171112236@gmail.com>
2023-02-26 20:56:37 +0530

    show upload progress and canceling on uploading player zip and restrict zip and csv files

--- src/components/manageEntities/ImportPlayerForm.js
+++ src/components/manageEntities/ImportPlayerForm.js
@@ -1,7 +1,9 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import axios from 'axios'
 
 import classes from './form.module.css'
+import CancelIcon from '@mui/icons-material/Cancel'
+import { IconButton, Tooltip } from '@mui/material'
 
 // constants
 const BASE_URL = process.env.REACT_APP_BASE_URL || ''
@@ -12,6 +14,17 @@ const ImportPlayersForm = (props) => {
   const csvRef = useRef()
   const zipRef = useRef()
   const [errors, setErrors] = useState([])
+  const [progress, setProgress] = useState(null)
+  const [uploadController, setUploadController] = useState(null)
+
+  const cancelUploadHandler = () => {
+    if (uploadController) uploadController.abort()
+    setProgress(null)
+  }
+
+  useEffect(() => {
+    cancelUploadHandler()
+  }, [])
 
   const formSubmitHandler = (e) => {
     e.preventDefault()
@@ -22,11 +35,20 @@ const ImportPlayersForm = (props) => {
     importFormData.append('csv', csvRef.current.files[0])
     importFormData.append('zip', zipRef.current.files[0])
 
+    const controller = new AbortController()
+    setUploadController(controller)
+
     const config = {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
+      onUploadProgress: (event) => {
+        const percentCompleted = Math.round((event.loaded * 100) / event.total)
+        setProgress(percentCompleted)
+      },
+      signal: controller.signal,
     }
+
     // POST request to add team
     const api = BASE_URL + '/api/v1/admin/player/import'
     axios
@@ -35,12 +57,14 @@ const ImportPlayersForm = (props) => {
         // close overlay and refresh the teams
         props.onCloseOverlay()
         props.onRefresh()
+        setProgress(null)
       })
       .catch((err) => {
-        props.onRefresh()
         if (err.response?.data?.errors?.length > 0) {
           setErrors(err.response.data.errors)
         }
+        props.onRefresh()
+        setProgress(null)
       })
   }
 
@@ -48,7 +72,7 @@ const ImportPlayersForm = (props) => {
     <div className={classes['view-panel']}>
       <form className={classes.form} onSubmit={formSubmitHandler}>
         <div className={classes.input}>
-          <label htmlFor="location">Location</label>
+          <label htmlFor="location">Location (players will be replaced)</label>
           <select id="location" ref={locationRef}>
             {props.locations.map((location, i) => (
               <option key={i} value={location}>
@@ -59,14 +83,31 @@ const ImportPlayersForm = (props) => {
         </div>
         <div className={classes.input}>
           <label htmlFor="csv">Csv *</label>
-          <input id="csv" type="file" ref={csvRef} />
+          <input id="csv" type="file" accept=".csv" ref={csvRef} required />
         </div>
         <div className={classes.input}>
           <label htmlFor="zip">images zip</label>
-          <input id="zip" type="file" ref={zipRef} />
+          <input id="zip" type="file" accept=".zip" ref={zipRef} />
         </div>
         <button type="submit">Import</button>
       </form>
+      {progress && (
+        <div className={classes['upload_stats']}>
+          <div className={classes['progress_bar']}>
+            <div>Uploading...</div>
+            <progress
+              className={classes['progress']}
+              value={progress}
+              max="100"
+            />
+          </div>
+          <Tooltip title="Cancel">
+            <IconButton onClick={cancelUploadHandler}>
+              <CancelIcon />
+            </IconButton>
+          </Tooltip>
+        </div>
+      )}
       {errors?.length > 0 && (
         <div className={classes['error__container']}>
           <div className={classes.heading}>Errors</div>
